a1b78c8640eab2fca80a2609aa31d106771962e0
Salar Soleimani <s.s_m1983@yahoo.com>
2021-04-22 21:59:36 -0400

    update the color of uialert actions

--- Sources/FDTake/FDTakeController.swift
+++ Sources/FDTake/FDTakeController.swift
@@ -1,6 +1,6 @@
 //
 //  FDTakeController.swift
-//  FDTake
+//  FDTakeExample
 //
 //  Copyright © 2015 William Entriken. All rights reserved.
 //
@@ -8,10 +8,47 @@
 import Foundation
 import MobileCoreServices
 import UIKit
-import Photos
 
-/// A class for selecting and taking photos
-open class FDTakeController: NSObject {
+/// User interface strings
+fileprivate enum FDTakeControllerLocalizableStrings: String {
+    /// Decline to proceed with operation
+    case cancel = "cancel"
+    
+    /// Option to select photo from library
+    case chooseFromLibrary = "chooseFromLibrary"
+    
+    /// Option to select photo from photo roll
+    case chooseFromPhotoRoll = "chooseFromPhotoRoll"
+    
+    /// There are no sources available to select a photo
+    case noSources = "noSources"
+    
+    /// Option to take photo using camera
+    case takePhoto = "takePhoto"
+    
+    /// Option to take video using camera
+    case takeVideo = "takeVideo"
+    
+    public func comment() -> String {
+        switch self {
+        case .cancel:
+            return "Decline to proceed with operation"
+        case .chooseFromLibrary:
+            return "Option to select photo/video from library"
+        case .chooseFromPhotoRoll:
+            return "Option to select photo from photo roll"
+        case .noSources:
+            return "There are no sources available to select a photo"
+        case .takePhoto:
+            return "Option to take photo using camera"
+        case .takeVideo:
+            return "Option to take video using camera"
+        }
+    }
+}
+
+/// A class for select and taking photos
+open class FDTakeController: NSObject /* , UIImagePickerControllerDelegate, UINavigationControllerDelegate*/ {
 
     // MARK: - Initializers & Class Convenience Methods
 
@@ -55,19 +92,19 @@ open class FDTakeController: NSObject {
     /// Enable selfie mode by default
     open var defaultsToFrontCamera = false
 
-    /// The UIBarButtonItem to present from (may be replaced by overloaded methods)
+    /// The UIBarButtonItem to present from (may be replaced by a overloaded methods)
     open var presentingBarButtonItem: UIBarButtonItem? = nil
 
-    /// The UIView to present from (may be replaced by overloaded methods)
+    /// The UIView to present from (may be replaced by a overloaded methods)
     open var presentingView: UIView? = nil
 
-    /// The UIRect to present from (may be replaced by overloaded methods)
+    /// The UIRect to present from (may be replaced by a overloaded methods)
     open var presentingRect: CGRect? = nil
 
-    /// The UITabBar to present from (may be replaced by overloaded methods)
+    /// The UITabBar to present from (may be replaced by a overloaded methods)
     open var presentingTabBar: UITabBar? = nil
 
-    /// The UIViewController to present from (may be replaced by overloaded methods)
+    /// The UIViewController to present from (may be replaced by a overloaded methods)
     open lazy var presentingViewController: UIViewController = {
         return UIApplication.shared.keyWindow!.rootViewController!
     }()
@@ -80,9 +117,6 @@ open class FDTakeController: NSObject {
 
     /// A video was selected
     open var didGetVideo: ((_ video: URL, _ info: [AnyHashable: Any]) -> Void)?
-    
-    // Last media fetched
-    open var didGetLastMedia: ((PHAsset?) -> Void)?
 
     /// The user did not attempt to select a photo
     open var didDeny: (() -> Void)?
@@ -113,9 +147,6 @@ open class FDTakeController: NSObject {
 
     /// Custom UI text (skips localization)
     open var takeVideoText: String? = nil
-
-    /// Custom UI text (skips localization)
-    open var lastVideoOrPhoto: String? = nil
 
 
     // MARK: - Private
@@ -148,19 +179,16 @@ open class FDTakeController: NSObject {
         } while true
     }
 
-    private func fetchLatestMedia() -> PHAsset? {
-        var asset: PHAsset?
-        let options = PHFetchOptions()
-        options.includeAssetSourceTypes = .typeUserLibrary
-        PHAsset.fetchAssets(with: options).enumerateObjects(options: .concurrent) { (assetReturned, _, _) in
-            asset = assetReturned
-        }
-        return asset
-    }
+    
     // MARK: - Localization
 
-    private func localizedString(for string: FDTakeControllerLocalizableStrings) -> String {
-        let bundleLocalization = string.localizedString
+    private func localizeString(_ string:FDTakeControllerLocalizableStrings) -> String {
+        let bundle = Bundle(for: type(of: self))
+        //let stringsURL = bundle.resourceURL!.appendingPathComponent("Localizable.strings")
+        let bundleLocalization = bundle.localizedString(forKey: string.rawValue, value: nil, table: nil)
+        //let a = NSLocal
+        //let bundleLocalization = NSLocalizedString(string.rawValue, tableName: nil, bundle: bundle, value: string.rawValue, comment: string.comment())
+        
         
         switch string {
         case .cancel:
@@ -175,8 +203,6 @@ open class FDTakeController: NSObject {
             return self.takePhotoText ?? bundleLocalization
         case .takeVideo:
             return self.takeVideoText ?? bundleLocalization
-        case .lastTakenMedia:
-            return self.lastVideoOrPhoto ?? bundleLocalization
         }
     }
 
@@ -197,19 +223,20 @@ open class FDTakeController: NSObject {
         if self.allowsSelectFromLibrary {
             if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
                 titleToSource.append((buttonTitle: .chooseFromLibrary, source: .photoLibrary))
-                titleToSource.append((buttonTitle: .lastTakenMedia, source: .photoLibrary))
             } else if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum) {
                 titleToSource.append((buttonTitle: .chooseFromPhotoRoll, source: .savedPhotosAlbum))
             }
         }
 
         guard titleToSource.count > 0 else {
-            let str = localizedString(for: .noSources)
+            let str = localizeString(.noSources)
 
             //TODO: Encapsulate this
-            //TODO: There has got to be a better way to do this
+            //TODO: These has got to be a better way to do this
             let alert = UIAlertController(title: nil, message: str, preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: localizedString(for: .cancel), style: .default, handler: nil))
+          let cancelAction = UIAlertAction(title: localizeString(.cancel), style: .default, handler: nil)
+          cancelAction.setValue(UIColor.darkGray, forKey: "titleTextColor")
+            alert.addAction(cancelAction)
 
             // http://stackoverflow.com/a/34487871/300224
             let alertWindow = UIWindow(frame: UIScreen.main.bounds)
@@ -227,7 +254,7 @@ open class FDTakeController: NSObject {
 
         alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
         for (title, source) in titleToSource {
-            let action = UIAlertAction(title: localizedString(for: title), style: .default) {
+            let action = UIAlertAction(title: localizeString(title), style: .default) {
                 (UIAlertAction) -> Void in
                 self.imagePicker.sourceType = source
                 if source == .camera && self.defaultsToFrontCamera && UIImagePickerController.isCameraDeviceAvailable(.front) {
@@ -244,34 +271,30 @@ open class FDTakeController: NSObject {
                 }
                 self.imagePicker.mediaTypes = mediaTypes
 
-                if title == .lastTakenMedia {
-                    let media = self.fetchLatestMedia()
-                    self.didGetLastMedia?(media)
-                } else {
-                    //TODO: Need to encapsulate popover code
-                    var popOverPresentRect: CGRect = self.presentingRect ?? CGRect(x: 0, y: 0, width: 1, height: 1)
-                    if popOverPresentRect.size.height == 0 || popOverPresentRect.size.width == 0 {
-                        popOverPresentRect = CGRect(x: 0, y: 0, width: 1, height: 1)
-                    }
-                    let topVC = self.topViewController(rootViewController: self.presentingViewController)
-                    
-                    if UI_USER_INTERFACE_IDIOM() == .phone || (source == .camera && self.iPadUsesFullScreenCamera) {
-                        topVC.present(self.imagePicker, animated: true, completion: nil)
-                    } else {
-                        // On iPad use pop-overs.
-                        self.imagePicker.modalPresentationStyle = .popover
-                        self.imagePicker.popoverPresentationController?.sourceRect = popOverPresentRect
-                        topVC.present(self.imagePicker, animated: true, completion: nil)
-                    }
+                //TODO: Need to encapsulate popover code
+                var popOverPresentRect: CGRect = self.presentingRect ?? CGRect(x: 0, y: 0, width: 1, height: 1)
+                if popOverPresentRect.size.height == 0 || popOverPresentRect.size.width == 0 {
+                    popOverPresentRect = CGRect(x: 0, y: 0, width: 1, height: 1)
                 }
-               
+                let topVC = self.topViewController(rootViewController: self.presentingViewController)
+
+                if UI_USER_INTERFACE_IDIOM() == .phone || (source == .camera && self.iPadUsesFullScreenCamera) {
+                    topVC.present(self.imagePicker, animated: true, completion: nil)
+                } else {
+                    // On iPad use pop-overs.
+                    self.imagePicker.modalPresentationStyle = .popover
+                    self.imagePicker.popoverPresentationController?.sourceRect = popOverPresentRect
+                    topVC.present(self.imagePicker, animated: true, completion: nil)
+                }
             }
+          action.setValue(UIColor(hex: 0xE80919), forKey: "titleTextColor")
             alertController!.addAction(action)
         }
-        let cancelAction = UIAlertAction(title: localizedString(for: .cancel), style: .cancel) {
+        let cancelAction = UIAlertAction(title: localizeString(.cancel), style: .cancel) {
             (UIAlertAction) -> Void in
             self.didCancel?()
         }
+      cancelAction.setValue(UIColor.darkGray, forKey: "titleTextColor")
         alertController!.addAction(cancelAction)
 
         let topVC = topViewController(rootViewController: presentingViewController)
@@ -295,17 +318,19 @@ open class FDTakeController: NSObject {
 }
 
 extension FDTakeController : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    public func imagePickerController(
-        _ picker: UIImagePickerController,
-        didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]
-    ) {
+    /// Conformance for ImagePicker delegate
+    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // Local variable inserted by Swift 4.2 migrator.
+        let info = convertFromUIImagePickerControllerInfoKeyDictionary(info)
+
         UIApplication.shared.isStatusBarHidden = true
-        switch info[.mediaType] as! CFString {
-        case kUTTypeImage:
-            let imageToSave: UIImage
-            if let editedImage = info[.editedImage] as? UIImage {
+        let mediaType: String = info[convertFromUIImagePickerControllerInfoKey(UIImagePickerController.InfoKey.mediaType)] as! String
+        var imageToSave: UIImage
+        // Handle a still image capture
+        if mediaType == kUTTypeImage as String {
+            if let editedImage = info[convertFromUIImagePickerControllerInfoKey(UIImagePickerController.InfoKey.editedImage)] as? UIImage {
                 imageToSave = editedImage
-            } else if let originalImage = info[.originalImage] as? UIImage {
+            } else if let originalImage = info[convertFromUIImagePickerControllerInfoKey(UIImagePickerController.InfoKey.originalImage)] as? UIImage {
                 imageToSave = originalImage
             } else {
                 self.didCancel?()
@@ -315,10 +340,8 @@ extension FDTakeController : UIImagePickerControllerDelegate, UINavigationContro
             if UI_USER_INTERFACE_IDIOM() == .pad {
                 self.imagePicker.dismiss(animated: true)
             }
-        case kUTTypeMovie:
-             self.didGetVideo?(info[.mediaURL] as! URL, info)
-        default:
-            break
+        } else if mediaType == kUTTypeMovie as String {
+            self.didGetVideo?(info[convertFromUIImagePickerControllerInfoKey(UIImagePickerController.InfoKey.mediaURL)] as! URL, info)
         }
 
         picker.dismiss(animated: true, completion: nil)
@@ -330,5 +353,58 @@ extension FDTakeController : UIImagePickerControllerDelegate, UINavigationContro
         picker.dismiss(animated: true, completion: nil)
         self.didDeny?()
     }
+    
+    // Helper function inserted by Swift 4.2 migrator.
+    private func convertFromUIImagePickerControllerInfoKeyDictionary(_ input: [UIImagePickerController.InfoKey: Any]) -> [String: Any] {
+        return Dictionary(uniqueKeysWithValues: input.map {key, value in (key.rawValue, value)})
+    }
+    
+    // Helper function inserted by Swift 4.2 migrator.
+    private func convertFromUIImagePickerControllerInfoKey(_ input: UIImagePickerController.InfoKey) -> String {
+        return input.rawValue
+    }
 }
 
+import UIKit
+
+public extension UIColor {
+  /**
+   Creates an UIColor from HEX Int in 0x363636"format
+   
+   - parameter hex: HEX Int in 0x363636 format
+   
+   - returns: UIColor from HexString
+   */
+  convenience init(hex: Int) {
+    self.init(
+      red: (hex >> 16) & 0xFF,
+      green: (hex >> 8) & 0xFF,
+      blue: hex & 0xFF
+    )
+  }
+  convenience init(red: Int, green: Int, blue: Int) {
+    assert(red >= 0 && red <= 255, "Invalid red component")
+    assert(green >= 0 && green <= 255, "Invalid green component")
+    assert(blue >= 0 && blue <= 255, "Invalid blue component")
+    
+    self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+  }
+  convenience init(hexString: String, alpha: CGFloat = 1) {
+    var cString:String = hexString.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+    
+    if (cString.hasPrefix("#")) { cString.removeFirst() }
+    
+    if ((cString.count) != 6) {
+      self.init(hexString: "ff0000") // return red color for wrong hex input
+      return
+    }
+    
+    var rgbValue: UInt64 = 0
+    Scanner(string: cString).scanHexInt64(&rgbValue)
+    
+    self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+              green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+              blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+              alpha: alpha)
+  }
+}
